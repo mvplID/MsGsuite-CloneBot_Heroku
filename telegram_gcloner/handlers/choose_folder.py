b7a95078735e422e7b62927fa3877721013f47d9
Fajar Fitrianto <60049222+fajarFWD@users.noreply.github.com>
2021-10-27 06:48:24 +0700

    Update choose_folder.py

--- telegram_gcloner/handlers/choose_folder.py
+++ telegram_gcloner/handlers/choose_folder.py
@@ -16,7 +16,7 @@ from utils.restricted import restricted
 logger = logging.getLogger(__name__)
 
 default_max_folders = 4
-default_max_folders_vip = 10
+default_max_folders_vip = 50
 
 udkey_folders = 'folder_ids'
 udkey_folders_cache = 'folder_ids_cache'
